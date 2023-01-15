21a161d764067c692a166444ac72b87a776ab049
SebiC98 <sebastiancioata98@gmail.com>
2023-01-15 11:00:48 +0200

    Base project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sebastian Cioată on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
